c7c2a1973cadb68ebc21d499ecde0ccb2ba76a3d
amelia9x <kieuanh.jp@gmail.com>
2025-01-09 16:34:44 +1100

    sql: first migration

--- API/Program.cs
+++ API/Program.cs
@@ -29,4 +29,15 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try {
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+} catch (Exception ex) {
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error has ocurred during migration");
+}
+
 app.Run();
