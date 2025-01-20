93ae99bf3ad358658f3d6f7690118365cdf8a5bc
amelia9x <kieuanh.jp@gmail.com>
2025-01-20 12:07:51 +1100

    update: seed file

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -12,19 +14,23 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<IEnumerable<Activity>>> {}
-        public class Handler : IRequestHandler<Query, Result<IEnumerable<Activity>>>
+        public class Query : IRequest<Result<IEnumerable<ActivityDto>>> { }
+        public class Handler : IRequestHandler<Query, Result<IEnumerable<ActivityDto>>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<Result<IEnumerable<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<IEnumerable<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activites = await _context.Activities.ToListAsync();
-                return Result<IEnumerable<Activity>>.Success(activites);
+                var activites = await _context.Activities
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+                return Result<IEnumerable<ActivityDto>>.Success(activites);
             }
         }
     }
